e1af251aec763ace26fb2cd0a54d6449579a3c1c
Forellko <dragonforce97q@gmail.com>
2022-11-27 15:48:43 +0300

    Add validation to auth router

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,28 +1,74 @@
 const { Router } = require('express');
-const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 const router = Router();
 
-router.post('/register', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post(
+  '/register',
+  [
+    check('email', 'Indvalid email').isEmail(),
+    check('password', 'Invalid password').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const candidate = await User.findOne({ email });
+      if (!errors.isEmpty())
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Invalid registration inputs',
+        });
 
-    if (candidate)
-      return res.status(400).json({ message: 'User already exist' });
+      const { email, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({ email, password: hashedPassword });
+      const candidate = await User.findOne({ email });
 
-    await user.save();
+      if (candidate)
+        return res.status(400).json({ message: 'User already exist' });
 
-    res.status(201).json({ message: 'User created' });
-  } catch (error) {
-    res.status(500).json({ message: 'wrong /register' });
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+
+      res.status(201).json({ message: 'User created' });
+    } catch (error) {
+      res.status(500).json({ message: 'wrong /register' });
+    }
   }
-});
+);
 
-router.post('/login', async (req, res) => {});
+router.post(
+  '/login',
+  [
+    check('email', 'Indvalid email').isEmail(),
+    check('password', 'Invalid password').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty())
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Invalid registration inputs',
+        });
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) return res.status(400).json({ message: 'User not find' });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch)
+        return res.status(400).json({ message: 'Invalid password' });
+    } catch (error) {
+      res.status(500).json({ message: 'wrong /login' });
+    }
+  }
+);
 
 module.exports = router;
