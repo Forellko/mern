ad172ed33289488dca8abb298062e1903e34931f
Forellko <dragonforce97q@gmail.com>
2022-11-27 18:17:15 +0300

    Add basic pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import 'materialize-css';
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="container">
+      <h2>hello</h2>
+    </div>
+  );
 }
 
 export default App;
