cc3423b57c20ff6085597356e19e14b52b3640e6
Forellko <dragonforce97q@gmail.com>
2022-11-27 19:58:13 +0300

    Add auto navigate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { RouterProvider, createBrowserRouter, Router } from 'react-router-dom';
 import 'materialize-css';
-
+import { LinksPage } from './pages/LinksPage';
+import { CreatePage } from './pages/CreatePage';
+import { AuthPage } from './pages/AuthPage';
+import { DetailPage } from './pages/DetailPage';
 function App() {
-  return (
-    <div className="container">
-      <h2>hello</h2>
-    </div>
-  );
+  let router = createBrowserRouter([
+    {
+      path: '/',
+      element: <AuthPage />,
+    },
+    {
+      path: '/links',
+      element: <LinksPage />,
+    },
+    {
+      path: '/create',
+      element: <CreatePage />,
+    },
+    {
+      path: '/detail/:id',
+      element: <DetailPage />,
+    },
+  ]);
+  const isAuth = true;
+  if (!isAuth) router.navigate('/');
+
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { redirect } from 'react-router-dom';
 
 export const AuthPage = () => {
+  redirect('/links');
+
   return (
     <div>
       <h1>Auth Page</h1>
