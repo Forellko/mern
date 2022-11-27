3abdcd5154482b18e4a0e726637d8e5efcaf53b7
Forellko <dragonforce97q@gmail.com>
2022-11-27 15:29:10 +0300

    Add user model

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,28 @@
 const { Router } = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 const router = Router();
 
-router.post()
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const candidate = await User.findOne({ email });
+
+    if (candidate)
+      return res.status(400).json({ message: 'User already exist' });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({ email, password: hashedPassword });
+
+    await user.save();
+
+    res.status(201).json({ message: 'User created' });
+  } catch (error) {
+    res.status(500).json({ message: 'wrong /register' });
+  }
+});
+
+router.post('/login', async (req, res) => {});
 
 module.exports = router;
