19cef10d83d5521388416b542e6b0314ececc148
Forellko <45753449+Forellko@users.noreply.github.com>
2022-11-29 21:14:09 +0300

    Comment mongoose connect

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const PORT = config.get('port') || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get('mongoUri'), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // await mongoose.connect(config.get('mongoUri'), {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
     app.listen(5000, () => console.log(`App has been started on port ${PORT}`));
   } catch (e) {
     console.log('Server Error', e.message);
